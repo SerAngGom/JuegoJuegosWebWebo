5dd6a01b6c5d36db2f1a846153d6926003d893eb
Bochipan <sara.ber.ara@gmail.com>
2023-04-27 17:26:01 +0200

    Update endscreen clean

--- js/endScreen.js
+++ js/endScreen.js
@@ -5,7 +5,7 @@ let endScreen = {
 
 function loadEndScreen() {
     game.load.image('bg', 'assets/imgs/background.png');
-    game.load.image('start', 'assets/imgs/start.png');
+    game.load.image('start', 'assets/imgs/start.png'); 
 }
 
 function createEndScreen() {
@@ -20,11 +20,8 @@ function createEndScreen() {
         infoText = game.add.text(TITLE_OFFSET, TITLE_OFFSET, 'STAGE ' + (stage) + ' COMPLETED', { font: 'Source Sans Pro', fontSize: '60px', fontWeight: 'bold', fill: color  } );
     }
 
-    infoText = game.add.text(TITLE_OFFSET, 7 * TITLE_OFFSET, 'ACCURACY: ' + calculateAccuracy() + '%', { font: 'Source Sans Pro', fontSize: '40px', fontWeight: 'bold', fill: color  } );
-
-    infoText = game.add.text(TITLE_OFFSET, 12 * TITLE_OFFSET, 'TYPED LETTERS: ' + typedLetters, { font: 'Source Sans Pro', fontSize: '25px', fill: color  } );
-
-    infoText = game.add.text(TITLE_OFFSET, 15 * TITLE_OFFSET, 'CORRECT LETTERS: ' + correctLetters, { font: 'Source Sans Pro', fontSize: '25px', fill: color  } );
+    infoText = game.add.text(TITLE_OFFSET, 12 * TITLE_OFFSET, 'SCORE: ' + score, { font: 'Source Sans Pro', fontSize: '25px', fill: color  } );
+    infoText = game.add.text(TITLE_OFFSET, 12 * TITLE_OFFSET, 'TIME: ' + time, { font: 'Source Sans Pro', fontSize: '25px', fill: color  } );
 
     //button for going  back to the start screen
     btnStart = game.add.button(20, 20, 'start', clickBackToStart);
@@ -32,7 +29,3 @@ function createEndScreen() {
     btnStart.position.setTo(GAME_AREA_WIDTH/2, 670);
 }
 
-function calculateAccuracy() {
-    if (typedLetters==0) return 0;
-    return ((correctLetters / typedLetters) * 100).toFixed(2);
-}
